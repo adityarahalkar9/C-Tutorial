fd57761fe3c5c19e1265c60b24e9de94bf3abf62
adityarahalkar9 <adityarahalkar9@gmail.com>
2024-09-06 18:38:14 +0530

    changes in datatypes.c

--- 3. Variables & Datatypes.c
+++ 3. Variables & Datatypes.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void Variables(){
+void variables(){
     int age = 24;
     float pi = 3.14;
     char hashtag = '#';
@@ -11,4 +11,25 @@ void Variables(){
     printf("You are %d years old\n", age);
     printf("%f\n",pi);
     printf("Hashtag is %c\n", hashtag);
+}
+
+void datatypes(){
+    /*
+        Primary:
+            -> Integer
+            -> Character
+            -> Floating Point
+            -> Double
+            -> Void
+        
+        Derived:
+            -> Function
+            -> Array
+            -> Pointer
+        
+        User Defined:
+            -> Struct
+            -> Union
+            -> Enum
+    */
 }
