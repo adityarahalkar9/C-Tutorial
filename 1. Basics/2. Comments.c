b9f3c55ce8799c16d02ee9d1ecd93813564b51f2
adityarahalkar9 <adityarahalkar9@gmail.com>
2024-10-05 17:00:04 +0530

    Minor changes

---
+++ 1. Basics/2. Comments.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Escape Sequences
+/*
+    1. \a  :  Audible signal
+    2. \b  :  Backspace
+    3. \t  :  Tab
+    4. \n  :  Newline
+    5. \v  :  Vertical tab
+    6. \f  :  New page / Clear screen
+    7. \r  :  Carriage return
+*/
+
+// Type specifiers
+/*
+    1. %c     :  For single characters
+    2. %d,%i  :  For integer values
+    3. %e,%f  :  For floating point numbers
+    4. %lf    :  For double 
+    5. %o     :  For octal numbers
+    6. %s     :  For a sequence of characters(strings)
+    7. %u     :  For unsigned integer values
+    8. %x,%X  :  For hexadecimal values
+*/
